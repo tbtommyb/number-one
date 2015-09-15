041590153042b63254c47ae66a0f6d03e79192ce
Tom Barrett <tbtommyb@gmail.com>
2015-09-15 08:43:42 +0100

    appears to get empty array from server

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,62 @@
-/*global NumberOne, $*/
+$(function(){
 
-
-window.NumberOne = {
-    Models: {},
-    Collections: {},
-    Views: {},
-    Routers: {},
-    init: function () {
-        var recordList = new NumberOne.Collections.Records();
-        recordList.reset();
-        var recordView = new NumberOne.Views.Record({
-            collection: recordList
-        });
-        //new this.Views.Record;
-    }
-};
-
-$(document).ready(function () {
     'use strict';
-    NumberOne.init();
+
+    var Record = Backbone.Model.extend({
+
+        initialize: function() {
+        },
+
+        defaults: {
+            title: 'Hi',
+            artist: 'There'
+        },
+
+        validate: function(attrs, options) {
+        },
+
+        parse: function(response, options)  {
+            return response;
+        }
+    });
+
+    var RecordCollection = Backbone.Collection.extend({
+
+        model: Record,
+
+        url: 'http://localhost:9000/api/records'
+
+    });
+
+    var RecordView = Backbone.View.extend({
+
+        //template: _.template('<h3><%=artist %>: <%=title %></h3>'),
+
+        el: '#record',
+
+        className: '',
+
+        events: {},
+
+        initialize: function () {
+            this.collection = new RecordCollection();
+            this.collection.fetch({reset:true});
+            this.render();
+
+            this.listenTo(this.collection, 'change', this.render);
+        },
+
+        render: function () {
+            var renderedContent = this.collection;
+            console.log(renderedContent);
+            this.$el.html(renderedContent);
+        }
+
+    });
+
+    var testCollection = new RecordCollection();
+    testCollection.reset();
+    var testView = new RecordView({
+        collection: testCollection
+    });
 });
