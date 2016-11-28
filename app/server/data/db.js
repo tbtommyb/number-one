e7164e0394f575a3833f4aa23f00b3fc97c470c3
Tom Barrett <tbtommyb@gmail.com>
2016-11-28 21:55:25 +0000

    Fixing module exports

--- app/server/data/db.js
+++ app/server/data/db.js
@@ -3,8 +3,6 @@
 var sqlite3 = require('sqlite3');
 var path = require('path');
 
-var db = new sqlite3.Database(path.join(__dirname, 'data.db'));
-
 module.exports = {
-    db
+    db: new sqlite3.Database(path.join(__dirname, 'data.db'))
 };
