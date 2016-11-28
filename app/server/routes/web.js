580f33f86eec075e7840be10f7d69e83949235f0
Tom Barrett <tbtommyb@gmail.com>
2016-11-28 23:53:40 +0000

    Refactoring web route

--- app/server/routes/web.js
+++ app/server/routes/web.js
@@ -1,5 +1,3 @@
-/* jslint node: true, nomen: true */
-
 'use strict';
 
 var express = require('express');
@@ -9,12 +7,12 @@ var router = express.Router();
 
 // Serve the client route ------------
 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '/../../public/index.html'));
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'public', 'index.html'));
 });
 
-router.use(function (err, req, res, next) {
-    res.sendFile(path.join(__dirname, '/../../public/404.html'));
+router.use(function(err, req, res, next) {
+    res.sendFile(path.join(__dirname, '..', '..', 'public', '404.html'));
 });
 
 module.exports = function() { return router; };
