8daf034585073a9adfe36a50a431325e7e8a959a
Tom Barrett <tbtommyb@gmail.com>
2016-12-04 18:17:03 +0000

    Removed unnecessary files

--- dist/server/server/startServer.js
+++
@@ -1,9 +0,0 @@
-const app = require('./server.js');
-
-/*** Initialise ***/
-const port = process.env.PORT || 9000;
-console.log('initialising server on port ', port);
-
-const server = app.listen(port);
-
-module.exports = server;
